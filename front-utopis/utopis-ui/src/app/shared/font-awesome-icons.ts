92fe9713289b860b879031ce9482d54fc00012d3
Fuji-Vanilli <ijufillivan@gmail.com>
2024-06-27 18:13:56 +0000

    update

--- front-utopis/utopis-ui/src/app/shared/font-awesome-icons.ts
+++ front-utopis/utopis-ui/src/app/shared/font-awesome-icons.ts
@@ -1,5 +1,15 @@
-import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleQuestion,
+  faCircleUser,
+  faGear, faMagnifyingGlass,
+  faRightFromBracket,
+  IconDefinition
+} from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[]= [
-
+  faRightFromBracket,
+  faGear,
+  faCircleUser,
+  faCircleQuestion,
+  faMagnifyingGlass
 ]
