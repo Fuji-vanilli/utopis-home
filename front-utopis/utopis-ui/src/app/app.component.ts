72c1c719d2413368a97f31b6c7df77d77c26533d
Fuji-Vanilli <ijufillivan@gmail.com>
2024-06-27 16:38:45 +0000

    update

--- front-utopis/utopis-ui/src/app/app.component.ts
+++ front-utopis/utopis-ui/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'utopis-ui';
+export class AppComponent implements OnInit{
+
+  private faIconLibrary= inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
