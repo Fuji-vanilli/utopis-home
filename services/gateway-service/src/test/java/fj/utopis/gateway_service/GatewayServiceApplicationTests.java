d8252957c8d6475dac60f34a340b08e50d5e9185
Fuji-Vanilli <ijufillivan@gmail.com>
2024-06-28 08:51:29 +0000

    add gateway

---
+++ services/gateway-service/src/test/java/fj/utopis/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fj.utopis.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
