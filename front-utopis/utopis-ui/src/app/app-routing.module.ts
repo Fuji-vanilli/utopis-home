d3b8265da28b7a766e6289dce4f6fb706e1b3a7b
Fuji-Vanilli <ijufillivan@gmail.com>
2024-06-30 12:18:44 +0000

    add property service

--- front-utopis/utopis-ui/src/app/app-routing.module.ts
+++ front-utopis/utopis-ui/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { AuthGuard } from './guards/auth.guard';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'user', component: UserComponent, canActivate: [AuthGuard], data: {roles: ["ADMIN"]}}
 ];
 
---
+++ services/property-service/src/test/java/fj/utopis/property_service/PropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fj.utopis.property_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
